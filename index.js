f13911a32f84142cdd720ae8e328913b30c50c6c
ÞttmfxÞ <twambililemusukwa@gmail.com>
2025-08-01 16:18:20 +0200

    added theme toggle button

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+
+var isDark = false;
+
+$(".toggle-theme").on("click", function (){
+    if(isDark){
+    $("body").addClass("background-dark")
+    $(".toggle-theme").addClass("toggle-theme-dark")
+    $(".toggle-theme").removeClass("toggle-theme-light")
+
+    } else {
+        $("body").removeClass("background-dark")
+         $(".toggle-theme").removeClass("toggle-theme-dark")
+        $(".toggle-theme").addClass("toggle-theme-light")
+
+    }
+    isDark = !isDark
+})
 // TODO: Add event listener for theme change button
 
 // TODO: Add jQuery code for highlighting list items
